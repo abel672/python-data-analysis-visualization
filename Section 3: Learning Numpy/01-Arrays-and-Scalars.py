879138517f60b723d0e97db3865b4559c2d335ba
Abel Mescua <abelmescua@gmail.com>
2021-04-21 10:11:31 +0200

    Using arrays and scalars

---
+++ Section 3: Learning Numpy/01-Arrays-and-Scalars.py
@@ -0,0 +1,31 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import numpy as np
+
+
+# %%
+from __future__ import division
+
+
+# %%
+5/2
+
+
+# %%
+arr1 = np.array([[1,2,3,4],[8,9,10,11]])
+
+
+# %%
+arr1
+
+
+# %%
+# multiply array
+arr1*arr1
+
+
+# %%
+
+
+
