f03136d043c807921fba2f192b2588afa5f36f00
Abel Mescua <abelmescua@gmail.com>
2021-09-25 10:13:52 +0200

    Titanic Project part 2: Facade grids, Quest 2, 3

--- Section-8-Example-Projects/00-Titanic-Project.py
+++ Section-8-Example-Projects/00-Titanic-Project.py
@@ -58,3 +58,101 @@ sns.catplot(x='Sex', data=titanic_df, kind='count', hue='Pclass') # women and me
 sns.catplot(x='Pclass', data=titanic_df, kind='count', hue='Sex') # women and men by classes
 
 
+# %%
+def male_female_child(passenger):
+    age, sex = passenger
+
+    if age < 16:
+        return 'child'
+    else:
+        return sex
+
+
+# %%
+# Adding a 'person' column
+titanic_df['person'] = titanic_df[['Age','Sex']].apply(male_female_child, axis=1)
+
+
+# %%
+titanic_df[0:10]
+
+
+# %%
+sns.catplot(x='Pclass',data=titanic_df, kind='count',hue='person') # plot with the 'person' column
+
+
+# %%
+titanic_df['Age'].hist(bins=70) # age histogram
+
+
+# %%
+titanic_df['Age'].mean()
+
+
+# %%
+titanic_df['person'].value_counts()
+
+
+# %%
+# Creating facade grid
+fig = sns.FacetGrid(titanic_df,hue='person',aspect=4)
+fig.map(sns.kdeplot,'Age',shade=True)
+
+oldest = titanic_df['Age'].max()
+
+fig.set(xlim=(0,oldest))
+
+fig.add_legend()
+
+
+# %%
+fig = sns.FacetGrid(titanic_df,hue='Pclass',aspect=4)
+fig.map(sns.kdeplot,'Age',shade=True)
+
+oldest = titanic_df['Age'].max()
+
+fig.set(xlim=(0,oldest))
+
+fig.add_legend()
+
+
+# %%
+titanic_df.head()
+
+
+# %%
+# check in which decks the passengers were
+deck = titanic_df['Cabin'].dropna() # drop null values
+
+
+# %%
+deck.head()
+
+
+# %%
+levels = []
+
+for level in deck:
+    levels.append(level[0])
+
+cabin_df = DataFrame(levels)
+
+cabin_df.columns = ['Cabin']
+
+sns.catplot(x='Cabin',data=cabin_df,palette='winter_d', kind='count')
+
+
+# %%
+cabin_df = cabin_df[cabin_df.Cabin != 'T']
+sns.catplot(x='Cabin', kind='count', data=cabin_df, palette='summer')
+
+
+# %%
+titanic_df.head()
+
+
+# %%
+# where did the passengers came from?
+sns.catplot(x='Embarked',data=titanic_df,hue='Pclass',order=['C','Q','S'],kind='count')
+
+
