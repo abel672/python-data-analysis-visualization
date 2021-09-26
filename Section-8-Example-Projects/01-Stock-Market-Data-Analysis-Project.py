73798ecf471b2fdd6369f14f496142c1570c1674
Abel Mescua <abelmescua@gmail.com>
2021-09-26 08:39:55 +0200

    Stock Market Data Analysis part 2: Moving Averages

--- Section-8-Example-Projects/01-Stock-Market-Data-Analysis-Project.py
+++ Section-8-Example-Projects/01-Stock-Market-Data-Analysis-Project.py
@@ -61,3 +61,57 @@ AAPL['Adj Close'].plot(legend=True,figsize=(10,4))
 AAPL['Volume'].plot(legend=True,figsize=(10,4))
 
 
+# %%
+# Moving Averages
+ma_day = [10,20,50]
+
+for ma in ma_day:
+    column_name = "MA for %s days" %(str(ma))
+    AAPL[column_name] = AAPL['Adj Close'].rolling(ma).mean()
+
+
+# %%
+AAPL[['Adj Close','MA for 10 days','MA for 20 days','MA for 50 days']].plot(subplots=False,figsize=(10,4))
+
+
+# %%
+AAPL['Daily Return'] = AAPL['Adj Close'].pct_change()
+AAPL['Daily Return'].plot(figsize=(10,4),legend=True,linestyle='--',marker='o')
+
+
+# %%
+sns.displot(AAPL['Daily Return'].dropna(),bins=100,color='purple')
+
+
+# %%
+AAPL['Daily Return'].hist(bins=100)
+
+
+# %%
+closing_df = DataReader(tech_list,'yahoo',start,end)['Adj Close']
+
+
+# %%
+closing_df.head()
+
+
+# %%
+tech_rets = closing_df.pct_change()
+
+
+# %%
+tech_rets.head()
+
+
+# %%
+sns.jointplot(x='GOOG',y='GOOG',data=tech_rets,kind='scatter',color='seagreen')
+
+
+# %%
+sns.jointplot(x='GOOG',y='MSFT',data=tech_rets,kind='scatter')
+
+
+# %%
+url = 'https://en.wikipedia.org/wiki/Pearson_correlation_coefficient'
+
+
