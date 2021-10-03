e103ee7871836962df9fcf457d701b0784fa8545
Abel Mescua <abelmescua@gmail.com>
2021-10-03 12:26:49 +0200

    Logistic Regression part 4: Multicollinearity Consideration

--- Section-9-Machine-Learning/01-Supervised-Learning-Logistic-Regression.py
+++ Section-9-Machine-Learning/01-Supervised-Learning-Logistic-Regression.py
@@ -181,8 +181,105 @@ Y = df.Had_Affair
 
 Y.tail()
 
+# %% [markdown]
+# Part 6: Multicollinearity Consideration.
+# 
+# Now we need to get rid of a few columns. We will be dropping the occ1 and hocc1 columns to avoid [multicollinearity](https://en.wikipedia.org/wiki/Multicollinearity#Remedies_for_multicollinearity). Multicollinearity occurs due to the dummy variables) we created. This is because the [dummy variables](https://en.wikipedia.org/wiki/Dummy_variable_%28statistics%29) are highly correlated, our model begins to get distorted because one of the dummy variables can be linearly predicted from the others. We take care of this problem by dropping one of the dummy variables from each set, we do this at the cost of losing a data set point.
+# 
+# The other column we will drop is the affairs column. This is because it is basically a repeat of what will be our Y target, instead of 0 and 1 it just has 0 or a number, so we'll need to drop it for our target to make sense.
 
 # %%
+# Droping one column of each dummy variable set to avoid multicollinearity
+X = X.drop('occ1',axis=1)
+X = X.drop('hocc1',axis=1)
+
+# Drop affairs column so Y target make sense
+X = X.drop('affairs',axis=1)
 
 
+# %%
+X.head()
+
+
+# %%
+Y.head()
+
+# %% [markdown]
+# In order to use the Y with SciKit Learn, we need to set it as a 1-D array. This means we need to "flatten" the array. Numpy has a built in method for this called [ravel](https://numpy.org/doc/stable/reference/generated/numpy.ravel.html). Let's use it!
+
+# %%
+Y = np.ravel(Y)
+
+Y
+
+
+# %%
+log_model = LogisticRegression(solver='lbfgs', max_iter=1000)
+
+log_model.fit(X,Y)
+
+log_model.score(X,Y) # 73% accuracy
+
+# %% [markdown]
+# Looks like we got a 73% accuracy rating. Let's go ahead and compare this to the original Y data. We can do this by simply taking the mean of the Y data, since it is in the format 1 or 0, we can use the mean to calulate the percentage of women who reported having affairs. This is known as checking the [null error rate](https://www.youtube.com/watch?v=a_l991xUAOU&ab_channel=365DataScience).
+# 
+# 
+
+# %%
+Y.mean() # Percentage of women that had affairs
+
+
+# %%
+coeff_df = DataFrame(zip(X.columns,np.transpose(log_model.coef_)))
+
+
+# %%
+coeff_df # 0 =  Increment of the value, 1 = increment of the affair accordingly (negative or positive)
+
+# %% [markdown]
+# 
+# Looking at the coefficients we can see that a positive coeffecient corresponds to increasing the [likelihood](https://www.youtube.com/watch?v=XepXtl9YKwc&ab_channel=StatQuestwithJoshStarmer) of having an affair while a negative coefficient means it corresponds to a decreased likelihood of having an affair as the actual data value point increases.
+# 
+# As you might expect, an increased marriage rating corresponded to a decrease in the likelihood of having an affair. Increased religiousness also seems to correspond to a decrease in the likelihood of having an affair.
+# 
+# Since all the dummy variables (the wife and husband occupations) are positive that means the lowest likelihood of having an affair corresponds to the baseline occupation we dropped (1-Student).
+# %% [markdown]
+# ## Part 8: Testing and Training Data Sets¶
+# 
+# Just like we did in the Linear Regression Lecture, we should be splitting our data into training and testing data sets. We'll follow a very similar procedure to the Linear Regression Lecture by using SciKit Learn's built-in train_test_split method.
+
+# %%
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y)
+
+
+# %%
+log_model2 = LogisticRegression(solver='lbfgs', max_iter=1000)
+
+log_model2.fit(X_train, Y_train)
+
+
+# %%
+class_predict = log_model2.predict(X_test)
+
+
+# %%
+# Compare the predicted classes to the actual test classes
+print(metrics.accuracy_score(Y_test,class_predict))
+
+# %% [markdown]
+# Now we have a 73.35% accuracy score, which is basically the same as our previous accuracy score, 72.58%.
+# %% [markdown]
+# ## Part 9: Conclusion and more Resources
+# 
+# So what could we do to try to further improve our Logistic Regression model? We could try some [regularization techniques](https://en.wikipedia.org/wiki/Regularization_%28mathematics%29#Regularization_in_statistics_and_machine_learning) or using a non-linear model.
+# 
+# I'll leave the Logistic Regression topic here for you to explore more possibilites on your own. Here are several more resources and tutorials with other data sets to explore:
+# 
+# 1.) Here's another great post on how to do logistic regression analysis using Statsmodels from yhat!
+# 
+# 2.) The SciKit learn Documentation includes several [examples](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html) at the bottom of the page.
+# 
+# 3.) DataRobot has a great [overview](https://www.datarobot.com/blog/classification-with-scikit-learn/) of Logistic Regression
+# 
+# 4.) Fantastic resource from [aimotion.blogspot](http://aimotion.blogspot.com/2011/11/machine-learning-with-python-logistic.html) on the Logistic Regression and the Mathematics of how it relates to the cost function and gradient!
 
