0f8b04f5a53b06b1520769d570332990751115f9
Abel Mescua <abelmescua@gmail.com>
2021-10-16 14:27:33 +0200

    Hyphotesis Testing and Confidence Intervals

---
+++ Section-10-Stadistics/07-Hypothesis-Testing-and-Confidence-Intervals.py
@@ -0,0 +1,10 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# [Conficence Intervals](https://www.youtube.com/watch?v=hlM7zdf7zwU&ab_channel=KhanAcademy)
+# 
+# [Hypothesis Testing and P-value](https://www.youtube.com/watch?v=-FtlH4svqx4&ab_channel=KhanAcademy)
+# 
+# 
+# [Notebook](https://github.com/jmportilla/Statistics-Notes/blob/master/Hypothesis%20Testing%20and%20Confidence%20Intervals.ipynb)
+
