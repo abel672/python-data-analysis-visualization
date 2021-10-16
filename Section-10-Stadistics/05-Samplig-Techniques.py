aaf0da2e34e5931b6b77f9f9b5206ec35fb11ec5
Abel Mescua <abelmescua@gmail.com>
2021-10-16 09:34:34 +0200

    Sampling Techniques

---
+++ Section-10-Stadistics/05-Samplig-Techniques.py
@@ -0,0 +1,7 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# [Sample Distribution](https://www.youtube.com/watch?v=z0Ry_3_qhDw&ab_channel=KhanAcademy)
+# 
+# [Notebook](https://github.com/jmportilla/Statistics-Notes/blob/master/Sampling%20Techniques.ipynb)
+
